1ea89edf974b0f65ae63370640995f8ae0ccf643
Anne Chinn <achinn@nwlink.com>
2021-10-14 19:10:39 -0700

    first commit

---
+++ candidate_testing/script.js
@@ -0,0 +1,27 @@
+
+function askForName() {
+}
+
+function askQuestions() {
+}
+
+function gradeQuiz() {
+}
+
+function displayResults() {
+}
+
+
+
+// ************ BEGIN: DO NOT REMOVE THESE LINES **************//
+let btn = document.getElementById("btn");
+btn.addEventListener("click", ()=> { runProgram(); })
+
+function runProgram() {
+  askForName();
+  askQuestions();
+  gradeQuiz();
+  displayResults();
+}
+// ************ END: DO NOT REMOVE THESE LINES **************//
+
---
+++ scrabble_scrorer/script.js
@@ -0,0 +1,33 @@
+const scrabblePointSystem = {
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"],
+};
+
+function transformScrabblePointSystem() {
+}
+
+function calculateScoreTraditional(word) {
+}
+
+function calculateScoreVowelBonus(word) {
+}
+
+function calculateScoreSinglePoint(word) {
+  
+}
+
+function runProgram() {
+}
+
+// ************ BEGIN: DO NOT REMOVE THESE LINES **************//
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  runProgram();
+});
+
+// ************ END: DO NOT REMOVE THESE LINES **************//
